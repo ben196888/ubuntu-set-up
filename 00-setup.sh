37695583c98e7e17a40c6d74c1ee9ce5c0cd5b52
ben196888 <ben196888@gmail.com>
2016-09-06 00:02:57 +0800

    Update

--- 00-setup.sh
+++ 00-setup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# Set env
+USERNAME=`whoami`
+
+# update pkg
 sudo apt-get update -yq
 
 # install git vim
 sudo apt-get install -yq git vim
+
 # setup git config
 git config --global user.email "ben196888@gmail.com"
 git config --global user.name "ben196888"
 git config --global push.default simple
+
+
 # nodejs
 sudo apt-get install -yq nodejs npm
 # setup node
@@ -18,9 +25,9 @@ sudo apt-get install -yq python python-pip
 sudo apt-get install -yq docker.io docker-compose
 # fish
 sudo apt-get install -yq fish
-sudo chown -R `whoami`. ~/.config/
+sudo chown -R $USERNAME. ~/.config/
 # set fish as default shell
-chsh -s `which fish`
+sudo chsh -s `which fish` $USERNAME
 curl -L http://get.oh-my.fish | fish
 
 # Require logout and re-login
