0476d4588e6b485e4ab4e812506fd37285d1971b
ben196888 <ben196888@gmail.com>
2016-09-06 00:39:05 +0800

    Update

--- 00-basic-setup.sh
+++ 00-basic-setup.sh
@@ -14,7 +14,6 @@ git config --global user.email "ben196888@gmail.com"
 git config --global user.name "ben196888"
 git config --global push.default simple
 
-
 # nodejs
 sudo apt-get install -yq nodejs npm
 # setup node
@@ -23,9 +22,14 @@ sudo ln -s `which nodejs` /usr/bin/node
 sudo apt-get install -yq python python-pip
 # docker
 sudo apt-get install -yq docker.io docker-compose
+
 # fish
 sudo apt-get install -yq fish
 sudo chown -R $USERNAME. ~/.config/
+# fisherman, a plugin manager for fish
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+# bass
+fisher edc/bass
 # set fish as default shell
 sudo chsh -s `which fish` $USERNAME
 curl -L http://get.oh-my.fish | fish
